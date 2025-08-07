a4716f488c14de0bcc0a9517069ef141c886c4c2
brant <brant@email.com>
2025-08-07 20:59:24 -0300

    atualização dos itens do blog

--- historias_sobre_Deus/models.py
+++ historias_sobre_Deus/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Recipe(models.Model):
+ title = models.CharField(max_length=200)
+ texto = models.TextField()
+ photo = models.ImageField()
+
+def __str__(self):
+ return self.title
